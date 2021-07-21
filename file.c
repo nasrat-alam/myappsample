27fb6c6422867a89e06ea3c35bbbaac748ea99b6
Nasrat Alam <nasrat.alam@alifsemi.com>
2021-07-21 03:46:19 -0700

    file.c is modified 3rd time

--- file.c
+++ file.c
@@ -11,5 +11,5 @@ int print_myName()
 {
 	printf("\nMy name is Nasrat");	
 	printf("\nMy name again is Nasrat");	
-	printf("\nMy name once again is Nasrat");	
+	printf("\nMy name once again is Nasrat Alam"));	
 }
